526f303e96ca103bab51d9a625c0ea995d3a4c9b
YASH MUNDHADA <ymundhada12@gmail.com>
2025-01-22 16:23:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,21 +15,8 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          {' '}
-          hello{' '}
+          Hello
         </a>
-        <Button
-          variant="contained"
-          style={{
-            color: 'green',
-            backgroundColor: 'yellow',
-            width: '500px',
-          }}
-          onClick={() => {
-            console.log('hello');
-          }}>
-          Hello world
-        </Button>
       </header>
     </div>
   );
